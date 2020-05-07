eb84a1129642e427f381abacd3da426581bddd01
Areeb <areeb.royepen@gmail.com>
2020-05-07 14:46:47 +0200

    initial tests

---
+++ src/test/java/com/example/Burial/Scheme/REST/API/BurialSchemeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Burial.Scheme.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurialSchemeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
