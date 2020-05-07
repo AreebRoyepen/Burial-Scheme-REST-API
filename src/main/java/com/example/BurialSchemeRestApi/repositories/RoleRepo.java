31e74dd6164e37d7b6476a08d72090d10ae0cb86
Areeb <areeb.royepen@gmail.com>
2020-05-07 14:24:30 +0200

    finalizing skeleton

---
+++ src/main/java/com/example/BurialSchemeRestApi/repositories/RoleRepo.java
@@ -0,0 +1,9 @@
+package com.example.BurialSchemeRestApi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.BurialSchemeRestApi.models.Role;
+
+public interface RoleRepo extends JpaRepository<Role, Long>{
+
+}
