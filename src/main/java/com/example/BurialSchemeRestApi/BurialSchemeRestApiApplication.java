f3cb66a4636757bdf78729b53c9bdaf0e2e73814
Areeb <areeb.royepen@gmail.com>
2020-05-07 14:00:12 +0200

    initial application

---
+++ src/main/java/com/example/BurialSchemeRestApi/BurialSchemeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.BurialSchemeRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurialSchemeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurialSchemeRestApiApplication.class, args);
+	}
+
+}
