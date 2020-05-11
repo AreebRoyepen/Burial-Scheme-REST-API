ee1e6d4395b8bb30bcbaaf906e56c606ea9c0945
Areeb <areeb.royepen@gmail.com>
2020-05-11 16:47:50 +0200

    added validation from business logic

--- src/main/java/com/example/BurialSchemeRestApi/controllers/UtilController.java
+++ src/main/java/com/example/BurialSchemeRestApi/controllers/UtilController.java
@@ -1,6 +1,10 @@
 package com.example.BurialSchemeRestApi.controllers;
 
+import com.example.BurialSchemeRestApi.models.Dependant;
+import com.example.BurialSchemeRestApi.models.Member;
+import com.example.BurialSchemeRestApi.models.Premium;
 import com.example.BurialSchemeRestApi.models.TransactionType;
+import com.example.BurialSchemeRestApi.repositories.PremiumRepo;
 import com.example.BurialSchemeRestApi.repositories.TransactionTypeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,13 +12,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @RestController
 @CrossOrigin
 public class UtilController {
 
+    @Autowired
+    PremiumRepo premiumRepo;
 
 
     public ResponseEntity<?> responseUtil(String message) {
@@ -23,11 +33,47 @@ public class UtilController {
         return ResponseEntity.status(HttpStatus.OK).body(m);
     }
 
-    public void bob (Long id){
+    public BigDecimal getTotalPremiums (Member member){
 
+        List<Premium> list = premiumRepo.findByMember(member);
+        BigDecimal total = BigDecimal.ZERO.setScale(2, RoundingMode.HALF_EVEN);
+        for (Premium p : list ) {
+            total.add(p.getAmount());
+        }
+        return total;
 
+    }
 
+    public boolean allDependantsClaimed(Member member){
+        Set<Dependant> set = member.getDependants();
 
+        int x = 0;
+        for (Dependant dependant: set) {
+            if(dependant.hasClaimed())
+                x ++;
+        }
+        if(x == set.size()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean lastDependantToClaim(Member member){
+        Set<Dependant> set = member.getDependants();
+
+        int x = 0;
+        for (Dependant dependant: set) {
+            if(dependant.hasClaimed())
+                x ++;
+        }
+        if(x == set.size() -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
--- src/main/java/com/example/BurialSchemeRestApi/repositories/PremiumRepo.java
+++ src/main/java/com/example/BurialSchemeRestApi/repositories/PremiumRepo.java
@@ -1,7 +1,12 @@
 package com.example.BurialSchemeRestApi.repositories;
 
+import com.example.BurialSchemeRestApi.models.Member;
 import com.example.BurialSchemeRestApi.models.Premium;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PremiumRepo extends JpaRepository<Premium, Long> {
+
+    List<Premium> findByMember(Member member);
 }
